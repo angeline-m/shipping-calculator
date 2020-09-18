cc9926acd0645e280667103483ca3bc669d859f1
angeline-m <shelite17@gmail.com>
2020-09-18 09:48:14 -0600

    Updated readme, HTML, and JS files

--- js/index.js
+++ js/index.js
@@ -1,24 +1,53 @@
-  /* 
- select the input element
- store a reference to the input element in a variable
- output the value  of your variable to the console.
- set a value for the shipping rate per 1kg store it in a variable
- 
- object.method()
- object.property()
- object.event()
+/*
+  getElementById(id attribute)
+  return value: element of group of elements markup
 
- let = allows you to overwrite the value
- const = one assignment only
+  const shippingweight = document.getElementById('weight')
+  console.log(shippingweight)
+*/
 
- document.getQuerySelector('input').value gives the value inside the text field
- 
- */
+/*
 
-const shippingWeight = document.getQuerySelector('input');
+  getElementByClassName(css class name)
+  return type HTMLCollection
+  ** class name does not have a dot
 
-console.log(shippingWeight);
+  const testThings = document.getElementsByClassName('test')
+  console.log(testThings)
 
-const shippingRate = 3;
+*/
 
-console.log('shipping weight is: ' + 3);
+/*  
+  getElementByTagName()
+
+  const testThings = document.getElementByTagName
+
+*/
+
+/*
+  Read data from the dom using a button click as the event that drives code
+
+*/
+
+//select the login button, add event listener click event, log to the console
+const loginButton = document.querySelector('button')
+loginButton.addEventListener('click', function() {
+  console.log('button clicked')
+})
+
+//reference variables
+//data types
+//string number boolean
+const calculateButton = document.querySelector('label');
+const shippingWeight = document.querySelector('input');
+const shippingPrice = document.querySelector('.shipping-price');
+const shippingRate = 5;
+
+
+calculateButton.addEventListener('click', function() {
+  const rate = Number(shippingWeight.value) * shippingRate;
+
+  shippingPrice.textContent = '$' + rate + '.00';
+  shippingPrice.style.fontSize = '4rem';
+})
+
